13acf414c7bd5605f2c75ef49b31260a4d7d8469
PAUTHIER Jonas <jonas.pauthier@gmail.com>
2019-10-08 14:01:49 +0200

    add @reach/router and dummy routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Router } from "@reach/router";
+import logo from "./logo.svg";
+import "./App.css";
+
+function Home() {
+  return <div>Hello i'm HOME</div>;
+}
+
+function Page1() {
+  return <div>Hello i'm Page1</div>;
+}
 
 function App() {
   return (
@@ -19,6 +28,10 @@ function App() {
           Learn React
         </a>
       </header>
+      <Router>
+        <Home path="/"></Home>
+        <Page1 path="/page1"></Page1>
+      </Router>
     </div>
   );
 }
